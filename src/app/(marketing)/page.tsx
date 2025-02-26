11bdd1c362685bf8109cb1deaadfbaafcca412e4
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-02-26 17:49:55 +0200

    Add section 3 - Pricing cards

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -71,6 +71,68 @@ export default function HomePage() {
           </div>
         </div>
       </section>
+      <section id="pricing" className="px-8 py-16 bg-accent/5">
+        <h2 className="text-4xl text-center text-balance font-semibold mb-8">
+          Pricing software which pays for itself 20x over
+        </h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto">
+          {subscriptionTiersInOrder.map((tier) => (
+            <PricingCard key={tier.name} {...tier} />
+          ))}
+        </div>
+      </section>
     </>
+  );
+}
+
+function PricingCard({
+  name,
+  priceInCents,
+  maxNumberOfProducts,
+  maxNumberOfVisits,
+  canAccessAnalytics,
+  canCustomizeBanner,
+  canRemoveBranding,
+}: (typeof subscriptionTiersInOrder)[number]) {
+  const isMostPopular = name === "Standard";
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="text-accent font-semibold mb-8">{name}</div>
+        <CardTitle className="text-xl font-bold">${priceInCents / 100} /mo</CardTitle>
+        <CardDescription>
+          {formatCompactNumber(maxNumberOfVisits)} pricing page visits/mo
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <SignUpButton>
+          <Button
+            className="text-lg w-full rounded-lg"
+            variant={isMostPopular ? "accent" : "default"}
+          >
+            Get Started
+          </Button>
+        </SignUpButton>
+      </CardContent>
+      <CardFooter className="flex flex-col gap-4 items-start">
+        <Feature className="font-bold">
+          {maxNumberOfProducts} {maxNumberOfProducts === 1 ? "product" : "products"}
+        </Feature>
+        <Feature>PPP Discounts</Feature>
+        {canCustomizeBanner && <Feature>Banner customization</Feature>}
+        {canAccessAnalytics && <Feature>Advanced analytics</Feature>}
+        {canRemoveBranding && <Feature>Remove Easy PPP branding</Feature>}
+      </CardFooter>
+    </Card>
+  );
+}
+
+function Feature({ children, className }: { children: ReactNode; className?: string }) {
+  return (
+    <div className={cn("flex items-center gap-2", className)}>
+      <CheckIcon className="size-4 stroke-accent bg-accent/25 rounded-full p-0.5" />
+      <span>{children}</span>
+    </div>
   );
 }
