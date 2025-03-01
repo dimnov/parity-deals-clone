f37f9ee87823aee45986a33dc075660a0fb82d9d
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-03-01 22:35:53 +0200

    create client and server env

---
+++ src/data/env/server.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  emptyStringAsUndefined: true,
+  server: {
+    DATABASE_URL: z.string().url(),
+    CLERK_SECRET_KEY: z.string(),
+    CLERK_WEBHOOK_SECRET: z.string(),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET,
+  },
+});
