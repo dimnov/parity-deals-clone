611957adf81c72fc4177e7a04b1f57650cec436b
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-03-02 20:49:55 +0200

    Create product grid component

---
+++ src/app/dashboard/_components/ProductGrid.tsx
@@ -0,0 +1,84 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontalIcon } from "lucide-react";
+import Link from "next/link";
+import AddToSiteProductModalContent from "./AddToSiteProductModalContent";
+import { AlertDialog, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import DeleteProductAlertDialogContent from "./DeleteProductAlertDialogContent";
+
+function ProductGrid({
+  products,
+}: {
+  name: string;
+  url: string;
+  description?: string | null;
+  id: string;
+}[]) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </div>
+  );
+}
+
+export default ProductGrid;
+
+export function ProductCard({
+  id,
+  name,
+  url,
+  description,
+}: {
+  id: string;
+  name: string;
+  url: string;
+  description?: string | null;
+}) {
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex gap-2 justify-between items-end">
+          <CardTitle>
+            <Link href={`/dashboard/products${id}/edit`}>{name}</Link>
+          </CardTitle>
+          <Dialog>
+            <AlertDialog>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" className="size-8 p-0">
+                    <div className="sr-only">Action Menu</div>
+                    <MoreHorizontalIcon className="size-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem asChild>
+                    <Link href={`/dashboard/products/${id}/edit`}>Edit</Link>
+                  </DropdownMenuItem>
+                  <DialogTrigger asChild>
+                    <DropdownMenuItem>Add To Site</DropdownMenuItem>
+                  </DialogTrigger>
+                  <AlertDialogTrigger asChild>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </AlertDialogTrigger>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <DeleteProductAlertDialogContent id={id} />
+            </AlertDialog>
+            <AddToSiteProductModalContent id={id} />
+          </Dialog>
+        </div>
+        <CardDescription>{url}</CardDescription>
+      </CardHeader>
+      {description && <CardContent>{description}</CardContent>}
+    </Card>
+  );
+}
