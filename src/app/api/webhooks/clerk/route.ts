eb11b448d9b5fc45364a81a9de6e6de0ea513131
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-03-06 20:53:16 +0200

    remove comments

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -6,29 +6,24 @@ import { createUserSubscription } from "@/server/db/subscription";
 import { deleteUser } from "@/server/db/users";
 
 export async function POST(req: Request) {
-  // Create new Svix instance with secret
   const wh = new Webhook(env.CLERK_WEBHOOK_SECRET);
 
-  // Get headers
   const headerPayload = await headers();
   const svixId = headerPayload.get("svix-id");
   const svixTimestamp = headerPayload.get("svix-timestamp");
   const svixSignature = headerPayload.get("svix-signature");
 
-  // If there are no headers, error out
   if (!svixId || !svixTimestamp || !svixSignature) {
     return new Response("Error: Missing Svix headers", {
       status: 400,
     });
   }
 
-  // Get body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
   let event: WebhookEvent;
 
-  // Verify payload with headers
   try {
     event = wh.verify(body, {
       "svix-id": svixId,
@@ -42,7 +37,6 @@ export async function POST(req: Request) {
     });
   }
 
-  // Do something with payload
   switch (event.type) {
     case "user.created":
       {
