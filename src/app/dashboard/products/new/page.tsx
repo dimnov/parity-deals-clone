4a74b3b560314e9abef53cc6bd19d5941c141ea7
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-03-06 20:56:26 +0200

    Add HasPermission to new product page

--- src/app/dashboard/products/new/page.tsx
+++ src/app/dashboard/products/new/page.tsx
@@ -1,18 +1,26 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import PageWithBackButton from "../../_components/PageWithBackButton";
 import ProductDetailsForm from "../../_components/forms/ProductDetailsForm";
+import HasPermission from "@/components/HasPermission";
+import { canCreateProduct } from "@/server/db/permissions";
 
 function NewProductPage() {
   return (
     <PageWithBackButton pageTitle="Create Products" backButtonHref="/dashboard/products">
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-xl">Product Details</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <ProductDetailsForm />
-        </CardContent>
-      </Card>
+      <HasPermission
+        permission={canCreateProduct}
+        renderFallback
+        fallbackText="You have already created the maximum number of products. Try upgrading your account to create more."
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-xl">Product Details</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ProductDetailsForm />
+          </CardContent>
+        </Card>
+      </HasPermission>
     </PageWithBackButton>
   );
 }
