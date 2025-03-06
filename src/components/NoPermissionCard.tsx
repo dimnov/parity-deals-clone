6e038ce9afe34c761c8e8c8be89c687691228e32
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-03-06 19:52:12 +0200

    Create NoPermissionCard component

---
+++ src/components/NoPermissionCard.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+function NoPermissionCard({
+  children = "You do not have permission to perform this action. Try upgrading your account to access this feature.",
+}: {
+  children?: ReactNode;
+}) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Permission Denied</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>{children}</CardDescription>
+      </CardContent>
+      <CardFooter>
+        <Button asChild>
+          <Link href="/dashboard/subscription">Upgrade Account</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default NoPermissionCard;
