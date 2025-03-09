3e0e4af7ee750a211943c3d981f99723c8970570
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-03-09 19:38:54 +0200

    Create the internal banner component

---
+++ src/app/api/products/[productId]/banner/route.ts
@@ -0,0 +1,92 @@
+import Banner from "@/components/Banner";
+import { env } from "@/data/env/server";
+import { canRemoveBranding, canShowDiscountBanner } from "@/server/db/permissions";
+import { getProductForBanner } from "@/server/db/products";
+import { createProductView } from "@/server/db/productViews";
+import { headers } from "next/headers";
+import { notFound } from "next/navigation";
+import { NextRequest } from "next/server";
+import { createElement } from "react";
+
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<{ productId: string }> }
+) {
+  const { productId } = await context.params;
+  const headersMap = await headers();
+  const requestUrl = headersMap.get("referer") || headersMap.get("origin");
+
+  if (requestUrl == null) return notFound();
+
+  const countryCode = getCountryCode(request);
+
+  if (countryCode == null) return notFound();
+
+  const { product, discount, country } = await getProductForBanner({
+    id: productId,
+    countryCode,
+    url: requestUrl,
+  });
+
+  if (product == null) return notFound();
+
+  const canShowBanner = await canShowDiscountBanner(product.clerkUserId);
+
+  await createProductView({
+    productId: product.id,
+    countryId: country?.id,
+    userId: product.clerkUserId,
+  });
+  if (!canShowBanner) return notFound();
+  if (country == null || discount == null) return notFound();
+
+  return new Response(
+    await getJavaScript(product, country, discount, await canRemoveBranding(product.clerkUserId)),
+    { headers: { "content-type": "text-javascript" } }
+  );
+}
+
+function getCountryCode(request: NextRequest) {
+  const country = request.headers.get("x-vercel-ip-country");
+  if (country) return country;
+
+  if (process.env.NODE_ENV === "development") {
+    return env.TEST_COUNTRY_CODE;
+  }
+}
+
+async function getJavaScript(
+  product: {
+    customization: {
+      locationMessage: string;
+      bannerContainer: string;
+      backgroundColor: string;
+      textColor: string;
+      fontSize: string;
+      isSticky: boolean;
+      classPrefix: string | null;
+    };
+  },
+  country: { name: string },
+  discount: { coupon: string; percentage: number },
+  canRemoveBranding: boolean
+) {
+  const { renderToStaticMarkup } = await import("react-dom/server");
+
+  return `
+  const banner = document.createElement("div");
+  banner.innerHTML = '${renderToStaticMarkup(
+    createElement(Banner, {
+      message: product.customization.locationMessage,
+      mappings: {
+        country: country.name,
+        coupon: discount.coupon,
+        discount: (discount.percentage * 100).toString(),
+      },
+      customization: product.customization,
+      canRemoveBranding,
+    })
+  )}';
+  document.querySelector("${product.customization.bannerContainer}").prepend(...banner.children);
+  `.replace(/(\r\n|\n|\r)/g, "");
+}
