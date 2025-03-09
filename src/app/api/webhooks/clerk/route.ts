6d3f4bf39396c59c956f71d9af5e6ee60181af21
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-03-09 19:35:32 +0200

    Implement Stripe for subscriptions

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -2,8 +2,11 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { env } from "@/data/env/server";
-import { createUserSubscription } from "@/server/db/subscription";
+import { createUserSubscription, getUserSubscription } from "@/server/db/subscription";
 import { deleteUser } from "@/server/db/users";
+import { Stripe } from "stripe";
+
+const stripe = new Stripe(env.STRIPE_SECRET_KEY);
 
 export async function POST(req: Request) {
   const wh = new Webhook(env.CLERK_WEBHOOK_SECRET);
@@ -48,7 +51,15 @@ export async function POST(req: Request) {
       break;
     case "user.deleted":
       {
-        if (event.data.id != null) await deleteUser(event.data.id);
+        if (event.data.id != null) {
+          const userSubscription = await getUserSubscription(event.data.id);
+
+          if (userSubscription?.stripeSubscriptionId != null) {
+            await stripe.subscriptions.cancel(userSubscription.stripeSubscriptionId);
+          }
+
+          await deleteUser(event.data.id);
+        }
       }
       break;
 
