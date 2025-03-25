07736e1e65f79bee37998a957585f649751a3c25
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-03-25 12:41:29 +0200

    Add util for creating url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,25 @@ export function cn(...inputs: ClassValue[]) {
 export function removeTrailingSlash(path: string) {
   return path.replace(/\/$/, "");
 }
+
+export function createURL(
+  href: string,
+  oldParams: Record<string, string>,
+  newParams: Record<string, string | undefined>
+) {
+  const filteredOldParams = Object.fromEntries(
+    Object.entries(oldParams).filter(([_, value]) => value !== undefined)
+  );
+
+  const params = new URLSearchParams(filteredOldParams);
+
+  Object.entries(newParams).forEach(([key, value]) => {
+    if (value === undefined) {
+      params.delete(key);
+    } else {
+      params.set(key, value);
+    }
+  });
+
+  return `${href}?${params.toString()}`;
+}
